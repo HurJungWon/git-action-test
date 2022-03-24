78d254f9a93acbbe6b5fceb21bddafe532d7bb8b
jungwon <jungwon1423@gmail.com>
2022-03-25 04:53:39 +0900

    modified deploy path

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 APP_NAME=test
+DEPLOY_DIR=/root/jw/git-action/deploy
 
-EXIST_BLUE=$(docker-compose -p ${APP_NAME}-blue -f /root/jw/git-action/docker-compose-blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p ${APP_NAME}-blue -f ${DEPLOY_DIR}/docker-compose-blue.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
     IDLE_PORT=8000
-    docker-compose -p ${APP_NAME}-blue -f /root/jw/git-action/docker-compose-blue.yml up -d
+    docker-compose -p ${APP_NAME}-blue -f ${DEPLOY_DIR}/docker-compose-blue.yml up -d
 
 else
     echo "green up"
     IDLE_PORT=8001
-    docker-compose -p ${APP_NAME}-green -f /root/jw/git-action/docker-compose-green.yml up -d
+    docker-compose -p ${APP_NAME}-green -f ${DEPLOY_DIR}/docker-compose-green.yml up -d
 fi 
 
 echo "> Health check 시작합니다."
@@ -44,10 +45,10 @@ docker exec -it proxy sh /scripts/switch-serve.sh ${IDLE_PORT}
 docker exec -it proxy service nginx reload
 
 if [ -z "$EXIST_BLUE" ]; then
-    docker-compose -p ${APP_NAME}-green -f /root/jw/git-action/docker-compose-green.yml down
+    docker-compose -p ${APP_NAME}-green -f ${DEPLOY_DIR}/docker-compose-green.yml down
 
 else
-    docker-compose -p ${APP_NAME}-blue -f /root/jw/git-action/docker-compose-blue.yml down
+    docker-compose -p ${APP_NAME}-blue -f ${DEPLOY_DIR}/docker-compose-blue.yml down
 fi
 echo "> 배포 성공 Nginx Current Proxy Port: $IDLE_PORT"
 exit 0
