d43deb5d350ae127e956a562c58368ae532f065a
jungwon <jungwon1423@gmail.com>
2022-03-29 15:46:33 +0900

    modified git action registry

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 APP_NAME=test
-DEPLOY_DIR=/root/jw/git-action/deploy
+TEST_RANGE=100
 
-EXIST_BLUE=$(docker-compose -p ${APP_NAME}-blue -f ${DEPLOY_DIR}/docker-compose-blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -H ${NCP_HOSt} --tlscacert=${CA_CERT} --tlscert=${CERT} --tlskey=${KEY} --tlsverify -p ${APP_NAME}-blue -f docker-compose-blue.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
     IDLE_PORT=8000
-    docker-compose -p ${APP_NAME}-blue -f ${DEPLOY_DIR}/docker-compose-blue.yml up -d
+    docker-compose -H ${NCP_HOSt} --tlscacert=${CA_CERT} --tlscert=${CERT} --tlskey=${KEY} --tlsverify -p ${APP_NAME}-blue -f docker-compose-blue.yml up -d
 
 else
     echo "green up"
     IDLE_PORT=8001
-    docker-compose -p ${APP_NAME}-green -f ${DEPLOY_DIR}/docker-compose-green.yml up -d
+    docker-compose -H ${NCP_HOSt} --tlscacert=${CA_CERT} --tlscert=${CERT} --tlskey=${KEY} --tlsverify -p ${APP_NAME}-green -f docker-compose-green.yml up -d
 fi 
 
 echo "> Health check 시작합니다."
-echo "> curl -s http://0.0.0.0:$IDLE_PORT/health"
+echo "> curl -s http://0.0.0.0:${IDLE_PORT}/health"
 sleep 1
 
-for retry_count in {1..100}
+for retry_count in {1..${TEST_RANGE}}
 do
-response=$(curl -s http://0.0.0.0:$IDLE_PORT/health)
+response=$(curl -s http://0.0.0.0:${IDLE_PORT}/health)
 up_count=$(echo $response | grep 'UP' | wc -l)
 
 if [ $up_count -ge 1 ]
@@ -45,10 +45,10 @@ docker exec -it proxy sh /scripts/switch-serve.sh ${IDLE_PORT}
 docker exec -it proxy service nginx reload
 
 if [ -z "$EXIST_BLUE" ]; then
-    docker-compose -p ${APP_NAME}-green -f ${DEPLOY_DIR}/docker-compose-green.yml down
+    docker-compose -H ${NCP_HOSt} --tlscacert=${CA_CERT} --tlscert=${CERT} --tlskey=${KEY} --tlsverify -p ${APP_NAME}-green -f docker-compose-green.yml down
 
 else
-    docker-compose -p ${APP_NAME}-blue -f ${DEPLOY_DIR}/docker-compose-blue.yml down
+    docker-compose -H ${NCP_HOSt} --tlscacert=${CA_CERT} --tlscert=${CERT} --tlskey=${KEY} --tlsverify -p ${APP_NAME}-blue -f docker-compose-blue.yml down
 fi
 echo "> 배포 성공 Nginx Current Proxy Port: $IDLE_PORT"
 exit 0
